72fdf1c2207e41dc5a672090c46448c5f5e0fa57
asepac01 <lorelyangie01@gmail.com>
2021-06-12 23:36:57 -0500

    2 metodo usoDeVariables en la clase Sintaxis.

--- clase_07_06_2021.py
+++ clase_07_06_2021.py
@@ -1,12 +1,19 @@
-number = 96
-if type(number) == int:
-    print("» Resultado:", number * 2)
-else:
-    print("» Este dato NO es numérico.")
+# number = 96
+# if type(number) == int:
+#     print("» Resultado:", number * 2)
+# else:
+#     print("» Este dato NO es numérico.")
 
-def mensaje(hombre):
-    print(hombre)
+# def mensaje(hombre):
+#     print(hombre)
 
 
-mensaje("  ¤ Mi primer programa.")
-mensaje("  ¤ Mi segundo programa.")
+# mensaje("  ¤ Mi primer programa.")
+# mensaje("  ¤ Mi segundo programa.")
+
+
+class Sintaxis:
+    def usoDeVariables(self):
+        edad, _peso = 45, 50.75
+        print("» Edad: {}".format(edad))
+        print("» Peso: {}".format(_peso))
