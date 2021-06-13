b79d864f1ce2825078d5f9a2f1836410640b11d5
asepac01 <lorelyangie01@gmail.com>
2021-06-12 23:31:49 -0500

    1 probando parametros de una funcion.

--- clase_07_06_2021.py
+++ clase_07_06_2021.py
@@ -3,3 +3,10 @@ if type(number) == int:
     print("» Resultado:", number * 2)
 else:
     print("» Este dato NO es numérico.")
+
+def mensaje(hombre):
+    print(hombre)
+
+
+mensaje("  ¤ Mi primer programa.")
+mensaje("  ¤ Mi segundo programa.")
